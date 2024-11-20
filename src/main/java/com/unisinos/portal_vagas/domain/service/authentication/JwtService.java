b2f589faa5e1fe1e6be5224241e0527dab066480
Carolina <cpprates18@gmail.com>
2024-11-19 21:56:26 -0300

    update secret

--- src/main/java/com/unisinos/portal_vagas/domain/service/authentication/JwtService.java
+++ src/main/java/com/unisinos/portal_vagas/domain/service/authentication/JwtService.java
@@ -3,6 +3,7 @@ package com.unisinos.portal_vagas.domain.service.authentication;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +18,9 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class JwtService {
 
-    public static final String SECRET = "F047E11C6170E7FD415A7A6C2D80605EAF6190290216C8515835E4A16A447A559881A8616F11B93B54E3191FD3B49568E0D3D41614C661920A748E95E630B19A";
+    @Value("${jwt.secret}")
+    private String secret;
+
     private static final long VALIDITY = TimeUnit.MINUTES.toMillis(30);
 
     public String generateToken(UserDetails userDetails) {
@@ -33,7 +36,7 @@ public class JwtService {
     }
 
     private SecretKey generateKey() {
-        byte[] decodedKey = Base64.getDecoder().decode(SECRET);
+        byte[] decodedKey = Base64.getDecoder().decode(secret);
         return Keys.hmacShaKeyFor(decodedKey);
     }
 
